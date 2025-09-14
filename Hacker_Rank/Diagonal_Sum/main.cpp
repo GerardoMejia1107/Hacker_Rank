8e2b80118cd7785bfe4c74f8865d5ae8daf1f137
Gerardo233 <miguelmr1107@gmail.com>
2025-09-13 22:04:52 -0600

    Adding problem Diagonals substracction of a matrix

--- Hacker_Rank/Diagonal_Sum/main.cpp
+++ Hacker_Rank/Diagonal_Sum/main.cpp
@@ -1,22 +1,52 @@
-// Basic dependencies
 #include <iostream>
 #include <vector>
 using namespace std;
 
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+Complete the function diagonalDifference with the following parameter:
+int arr[n][m]: a 2-D array of integers
+Return:
+int: the absolute difference in sums along the diagonals
+*/
+
+int diagonalDifference(vector<vector<int>> arr, int lenght);
+
 int main()
 {
-  int rows = 10, columns = 10;
+  int rows = 4, columns = 4;
   vector<vector<int>> matrix(rows, vector<int>(columns, 0));
 
-  for (int i = 0; i < rows; i++)
-  {
+  matrix = {
+      // 0 1  2  3
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12},
+      {13, 14, 15, 16}};
 
-    for (int j = 0; j < columns; j++)
-    {
-      cout << matrix[i][j] << " ";
-    }
-    cout << endl;
-  }
+  cout << "The sum of the diagonals is: " << diagonalDifference(matrix, rows) << endl;
 
   return 0;
+}
+
+int diagonalDifference(vector<vector<int>> arr, int lenght)
+{
+  // This variables will store the sum of each side
+  int firstSide = 0, secondSide = 0;
+
+  // For the first diagonal both indexes will be the same, so we use the same iterator
+  for (int i = 0; i < lenght; i++)
+  {
+    firstSide += arr[i][i];
+  }
+
+  /* Fot the second diagonal, for the vertical we will iterate normally, but to get the right horizolntal index
+  we will substract the length of either the row or colum minus the current index minus ones.*/
+  for (int i = 0; i < lenght; i++)
+  {
+    secondSide += arr[i][lenght - i - 1];
+  }
+
+  // We return the absolute difference between both sides
+  return abs(firstSide - secondSide);
 }
